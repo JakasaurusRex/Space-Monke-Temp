4520d65067e536d1a43f6a7050b958ba99d8d7f3
Jake Torres <jakert2244@gmail.com>
2022-07-17 12:28:30 -0700

    Changed player movement and added it to the test encounter scene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,22 +1,41 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEditor.UI;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    
+    //Rotation around planet code
     public float movementSpeed = 600f;
-
     public Transform planet;
-    
     private float movement = 0f;
-    
-    // Update is called once per frame
-    void Update() {
-        movement = Input.GetAxisRaw("Horizontal");
+    [SerializeField] private float radius;
+    private float angleAroundPlanet;
+    public float minClamp;
+    public float maxClamp;
+
+    //Facing towards the planet
+    public float rotationModifier;
+    public float rotSpeed;
+
+    private void Start() {
+        angleAroundPlanet = 90;
     }
 
-    private void FixedUpdate() {
-        transform.RotateAround(new Vector3(planet.position.x, planet.position.y, 0), Vector3.forward, movement * Time.deltaTime * -movementSpeed);
+    void Update() {
+        //Player movement
+        movement = Input.GetAxisRaw("Horizontal");
+        angleAroundPlanet = Mathf.Clamp(angleAroundPlanet + movement * Time.deltaTime * -movementSpeed, minClamp, maxClamp);
+        Vector3 pos = (radius * UtilFunctions.DegreeToVector2(angleAroundPlanet));
+        transform.position = pos + planet.position;
+
+        //Facing planet code
+        Vector3 vectorToPlanet = transform.position - planet.transform.position;
+        float angle = Mathf.Atan2(vectorToPlanet.y, vectorToPlanet.x) * Mathf.Rad2Deg - rotationModifier;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * rotSpeed);
     }
     
 }
